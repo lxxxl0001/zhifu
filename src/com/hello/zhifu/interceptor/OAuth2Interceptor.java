e8a5b4839f171f2c11a60596838928ea968bd533
lxxxl0001 <lxxxl0001@163.com>
2017-02-25 15:39:15 +0800

    xxx

--- src/com/hello/zhifu/interceptor/OAuth2Interceptor.java
+++ src/com/hello/zhifu/interceptor/OAuth2Interceptor.java
@@ -4,11 +4,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.lang.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
-import com.hello.zhifu.model.UserInfo;
-import com.hello.zhifu.service.IUserInfoService;
 import com.hello.zhifu.utils.CookieUtils;
 import com.hello.zhifu.utils.WeChatUtils;
 
@@ -25,20 +22,7 @@ public class OAuth2Interceptor extends HandlerInterceptorAdapter {
             response.sendRedirect(WeChatUtils.getDomain() + "/oauth2Url?sourceUrl=" + sourceUrl);
 			return false;
 		} else {
-			String par = request.getParameter("pid");
-			if (par != null) {
-				UserInfo user = userService.selectByOpendId(openId);
-				request.getSession().setAttribute("user", user);
-				Integer pid = Integer.parseInt(par);
-				if (user.getParent() == null && user.getUserid() != pid) {
-					user.setParent(pid);
-					userService.update(user);
-				}
-			}
 			return true;
 		}
 	}
-	
-	@Autowired
-	private IUserInfoService userService;
 }
