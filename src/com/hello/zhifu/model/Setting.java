d6cb85f0a034fedc5bf0ecdc2c10856f7985ee0f
lxxxl0001 <lxxxl0001@163.com>
2017-02-25 16:10:10 +0800

    xxx

---
+++ src/com/hello/zhifu/model/Setting.java
@@ -0,0 +1,19 @@
+package com.hello.zhifu.model;
+
+public class Setting {
+	private Integer mkey;
+	private Double mvalue;
+	public Integer getMkey() {
+		return mkey;
+	}
+	public void setMkey(Integer mkey) {
+		this.mkey = mkey;
+	}
+	public Double getMvalue() {
+		return mvalue;
+	}
+	public void setMvalue(Double mvalue) {
+		this.mvalue = mvalue;
+	}
+	
+}
