b7a71e8e05d6f7790467a7050c2c5499c49e3369
lxxxl0001 <lxxxl0001@163.com>
2017-02-25 14:38:00 +0800

    xxx

--- src/com/hello/zhifu/controller/IndexController.java
+++ src/com/hello/zhifu/controller/IndexController.java
@@ -3,7 +3,6 @@ package com.hello.zhifu.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,36 +10,21 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.hello.zhifu.model.UserInfo;
-import com.hello.zhifu.service.IAwardService;
-import com.hello.zhifu.service.IUserInfoService;
-import com.hello.zhifu.utils.CookieUtils;
 import com.hello.zhifu.utils.SettingsUtil;
 
 @Controller
 public class IndexController {
 
-	@Autowired
-	private IUserInfoService userInfoService;
-	@Autowired
-	private IAwardService awardService;
-	
 	@RequestMapping(value = "/touzhu", method = RequestMethod.GET)
-	public String touzhu(Integer pid, ModelMap map, HttpServletRequest request) {
-		String openId = CookieUtils.getCookieValue(request, "openId");
-		UserInfo user = userInfoService.selectByOpendId(openId);
-		//更新上级id上级id不能是自己
-		if (user.getParent() == null && user.getUserid() != pid) {
-			user.setParent(pid);
-			userInfoService.update(user);
-		}
+	public String touzhu(ModelMap map, HttpServletRequest request) {
+		UserInfo user = (UserInfo)request.getSession().getAttribute("user");
 		map.put("userid", user.getUserid());
 		return "touzhu";
 	}
 	
 	@RequestMapping(value = "/erweima", method = RequestMethod.GET)
-	public String erweima(ModelMap map, HttpServletRequest request) {
-		String openId = CookieUtils.getCookieValue(request, "openId");
-		UserInfo user = userInfoService.selectByOpendId(openId);
+	public String erweima(Integer pid, ModelMap map, HttpServletRequest request) {
+		UserInfo user = (UserInfo)request.getSession().getAttribute("user");
 		map.put("userid", user.getUserid());
 		String domain = SettingsUtil.getInstance().getString("domain");
 		map.put("domain", domain);
@@ -49,8 +33,7 @@ public class IndexController {
 	
 	@RequestMapping(value = "/geren", method = RequestMethod.GET)
 	public String geren(ModelMap map, HttpServletRequest request) {
-		String openId = CookieUtils.getCookieValue(request, "openId");
-		UserInfo user = userInfoService.selectByOpendId(openId);
+		UserInfo user = (UserInfo)request.getSession().getAttribute("user");
 		map.put("user", user);
 		return "geren";
 	}
